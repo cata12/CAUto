a9a94d6cc54f57bf8eaa0b30dd00a36df08a46ef
Catalin Sadagurschi <catalin.sadagurschi@blackline.com>
2019-11-25 13:53:15 +0200

    Added the code from first week first exercise

--- MainMethod/Program.cs
+++ MainMethod/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace MainMethod
 {
@@ -10,6 +8,16 @@ namespace MainMethod
     {
         static void Main(string[] args)
         {
+            IList<string> nameList = new List<string>();
+            nameList.Add("anA");
+            nameList.Add("elena");
+            nameList.Add("MARIA");
+            nameList.Add("cRisTiAn");
+
+            //FirstExercise
+            foreach (var nume in nameList)
+                Console.WriteLine(CultureInfo.CurrentCulture.TextInfo.ToTitleCase(nume.ToLower()));
+            Console.ReadLine();
         }
     }
 }
